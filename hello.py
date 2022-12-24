acaeb201c8349611dc6ce0fff28085e97988084e
Swuizer <arghya143ghosh@gmail.com>
2022-12-24 20:51:39 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello World")
+# print(5)
+# print("bye")
+# print(17*13)
+
+print("Hello hey Fay\nHello hey Fay\nFay Fay Fay\nFay Fay Fay\nHello hey Fay\n")
+print(input("Reply it: "))
---
+++ main.py
@@ -0,0 +1,4 @@
+import pandas
+import hashlib
+# import tensorflow
+print("Hi")
