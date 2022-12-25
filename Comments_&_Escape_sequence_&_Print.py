4239b2af4b6ed945559dd0f087ca062cdcec88fe
Swuizer <arghya143ghosh@gmail.com>
2022-12-25 09:36:39 +0530

    Added Comments, Escape, Print

---
+++ Comments_&_Escape_sequence_&_Print.py
@@ -0,0 +1,9 @@
+# Hello brother
+print("Hey I am a \"good boy\"\nand my friends are also good boy.") # we are also add a comment in a middle of line
+
+# Multi Line comment alternative syntax ('''  ''')
+'''ok 
+ bro
+ bye'''
+
+print("Hey", 6, 7, sep="~", end="009\n")
