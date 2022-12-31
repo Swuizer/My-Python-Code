a759be8ebe4edf0376b02dab087481204709082d
Swuizer <arghya143ghosh@gmail.com>
2022-12-31 19:45:08 +0530

    Initial Commit

---
+++ #29_Dictionaries_Method.py
@@ -0,0 +1,14 @@
+ep = {122: 45, 123: 89, 567: 69, 670:69}
+ep2 = {222: 67, 566: 90}
+
+# ep.update(ep2)
+# ep.clear()
+# ep.pop(122)
+# print(ep)
+# empt = {}
+# print(type(empt))
+
+ep.popitem()
+# del ep
+del ep[122]
+print(ep)
