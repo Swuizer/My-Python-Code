9077dda1612a1b55a2dbc443af45758dc2d0ad0c
Swuizer <arghya143ghosh@gmail.com>
2023-01-09 23:12:40 +0530

    Initial commit

---
+++ #36_Enumerate_Function.py
@@ -0,0 +1,16 @@
+marks = [12, 56, 32, 98, 12, 45, 1, 4]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#         print("Harry, awesome")
+#     index += 1
+
+
+# Enumerate Function
+
+for index, mark in enumerate(marks):
+    print(mark)
+    if(index == 3):
+        print("Harry, awesome")
